8aa54cd809f9c56186ef45e48c31b41f3a128a89
ossan-engineer <bodega1123@gmail.com>
2025-12-26 20:28:15 +0900

    Remove react plugin and set package as module

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: 'node',
     include: ['**/*.test.ts', '**/*.test.tsx'],
